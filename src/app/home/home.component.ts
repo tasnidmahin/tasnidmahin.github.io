6a27142cd8ea406b16e3b95c8658386c49751dc9
Tasnid Mahin <tasnidmahin@gmail.com>
2024-10-20 17:18:32 +0600

    PORT_first_part: Add background video
    - Initialize Home component
    - Config route
    - Add background video for part 1
    - Add title name

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  @ViewChild('backgroundVideo') backgroundVideo!: ElementRef<HTMLVideoElement>;
+
+  ngOnInit(): void {}
+}
