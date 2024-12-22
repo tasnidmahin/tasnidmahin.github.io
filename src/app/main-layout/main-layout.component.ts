e6558901ec939d9a6c4e9e9c6b3afa7e5b7b6640
Tasnid Mahin <tasnidmahin@gmail.com>
2024-12-22 22:04:53 +0600

    About: Create about me component

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   standalone: true,
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
-  imports: [HomeComponent]
+  imports: [HomeComponent, AboutMeComponent]
 })
 export class MainLayoutComponent {
 
