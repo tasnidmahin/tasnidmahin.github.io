2323c16a91b7e8911113d43effcc8ea632105bd8
Tasnid Mahin <tasnidmahin@gmail.com>
2025-01-19 22:08:52 +0600

    Add new constants

--- src/app/main-layout/main.constants.ts
+++ src/app/main-layout/main.constants.ts
@@ -5,5 +5,8 @@ export const LANDING_TEXTS = {
 	A_SOFTWARE_ENGINEER: 'A Software Engineer',
 	A_FULL_STACK_DEVELOPER: 'A Full Stack Developer',
 	FULL_STACK_DEVELOPER: 'Full Stack Developer',
-	A_FRONT_END_DEVELOPER: 'A Front End Developer'
+	A_FRONT_END_DEVELOPER: 'A Front End Developer',
+	MY_SKILLS: 'My Skills',
+	FRONTEND_DEVELOPMENT: 'Frontend Development',
+	BACKEND_DEVELOPMENT: 'Backend Development',
 }
--- src/app/main-layout/skills/skills.component.ts
+++ src/app/main-layout/skills/skills.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LANDING_TEXTS } from '../main.constants';
 
 @Component({
   selector: 'app-skills',
@@ -9,5 +10,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-
+  public skills:string = LANDING_TEXTS.MY_SKILLS;
+  public frontend_development:string = LANDING_TEXTS.FRONTEND_DEVELOPMENT;
+  public backend_development:string = LANDING_TEXTS.BACKEND_DEVELOPMENT;
 }
