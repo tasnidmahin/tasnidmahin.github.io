32b47cc1b36c91aab3d755a9f350238fc0e44317
Tasnid Mahin <tasnidmahin@gmail.com>
2025-01-19 20:33:53 +0600

    Introduce skills to Main Layout component

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   standalone: true,
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
-  imports: [HomeComponent, AboutMeComponent]
+  imports: [HomeComponent, AboutMeComponent, SkillsComponent]
 })
 export class MainLayoutComponent {
 
