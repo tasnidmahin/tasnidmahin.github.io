316ed2f0860d76989df51bb84df2c6b7791fb7f2
tasnidmahin <tasnidmahin@gmail.com>
2025-05-31 18:43:44 +0600

    project: project card component created

---
+++ src/app/main-layout/projects/project-card/project-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-card',
+  standalone: true,
+  templateUrl: './project-card.component.html',
+  styleUrls: ['./project-card.component.scss']
+})
+export class ProjectCardComponent {
+
+}
--- src/app/main-layout/projects/projects.component.ts
+++ src/app/main-layout/projects/projects.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { ProjectCardComponent } from './project-card/project-card.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  imports: [
+    ProjectCardComponent
+  ]
 })
 export class ProjectsComponent {
 
